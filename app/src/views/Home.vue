35b37cec1c185ea7e0f8521c27e4b480eb2a20a3
hardcommitoneself <hardcommitoneself@gmail.com>
2023-06-06 10:50:03 +0200

    fix: home page

--- app/src/views/Home.vue
+++ app/src/views/Home.vue
@@ -1,3 +1,6 @@
 <template>
     <!-- home view -->
+    <div class="w-screen h-screen flex items-center justify-center">
+        Welcome to DIRS21! <router-link to="/dishes"><span class="cursor-pointer italic hover:underline">Go to system</span></router-link>
+    </div>
 </template>
