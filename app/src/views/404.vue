cbc592a5079f85ccee77f1232cf1a5656de87d15
hardcommitoneself <hardcommitoneself@gmail.com>
2023-06-06 15:52:14 +0200

    fix: hot fix 404 page

--- app/src/views/404.vue
+++ app/src/views/404.vue
@@ -1,3 +1,5 @@
 <template>
-    <!-- 404 Not found view -->
+  <div class="flex items-center justify-center w-full h-[80vh]">
+    <span class="text-2xl text-gray-500">404 Not Found</span>
+  </div>
 </template>
