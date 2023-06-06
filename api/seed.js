4def9f1f7c92797384e94451e4d1717f6e3d075f
hardcommitoneself <hardcommitoneself@gmail.com>
2023-06-06 12:44:03 +0200

    feat: seed dishes

---
+++ api/seed.js
@@ -0,0 +1,51 @@
+const { uuid } = require("uuidv4");
+const { faker } = require("@faker-js/faker");
+
+const randomDish = () => {
+  return {
+    id: uuid(),
+    name: faker.commerce.productName(),
+    description: faker.commerce.productDescription(),
+    price: parseFloat(faker.commerce.price()),
+    category: {
+      starter: faker.helpers.multiple(faker.commerce.productName, {
+        count: faker.number.int(3),
+      }),
+      main: faker.helpers.multiple(faker.commerce.productName, {
+        count: faker.number.int(5),
+      }),
+      dessert: faker.helpers.multiple(faker.commerce.productName, {
+        count: faker.number.int(2),
+      }),
+      beverage: faker.helpers.multiple(faker.commerce.productName, {
+        count: faker.number.int(2),
+      }),
+    },
+    images: {
+      lg: faker.image.urlLoremFlickr({
+        category: "food",
+        width: 560,
+        height: 560,
+      }),
+      md: faker.image.urlLoremFlickr({
+        category: "food",
+        width: 240,
+        height: 240,
+      }),
+      sm: faker.image.urlLoremFlickr({
+        category: "food",
+        width: 128,
+        height: 128,
+      }),
+    },
+    stock: faker.number.int(10),
+    active: true,
+    readyIn: faker.number.int(50),
+    _Created: new Date(),
+    _Changed: null,
+  };
+};
+
+exports.dishes = faker.helpers.multiple(randomDish, {
+  count: faker.number.int(100),
+});
